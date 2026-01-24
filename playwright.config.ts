cdaeefe0459250a3be66235766fa63f5d2fd8fcf
frayfray248 <nicman248@hotmail.com>
2026-01-24 17:57:27 +0000

    feat: Enhance MongoDB setup and initialization for local development environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx prisma generate && npm run build && npm run start',
+    command: 'npm run generate && npm run build && npm run start',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000, // 2 minutes
